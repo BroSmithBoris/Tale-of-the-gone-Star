acb371110e5c8d68ecc70305dc5a70dc2e574e0f
BroSmithBoris <47716016+BroSmithBoris@users.noreply.github.com>
2019-10-16 22:19:58 +0500

    Допиливаю скрипт прыжка

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -2,47 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Move : MonoBehaviour
 {
-    //KeyCode up = KeyCode.W;
-    //KeyCode down = KeyCode.S;
-    KeyCode left = KeyCode.A;
-    KeyCode right = KeyCode.D;
-    public float speed;
-    public float gravity = 20.0f;
-    //Animator anim;
+    #region
+    //KeyCode left = KeyCode.A;
+    //KeyCode right = KeyCode.D;
+    //KeyCode jump = KeyCode.Space;
 
+    //public float speed, jumpForce;
+    //public LayerMask groundLayers;
 
-    Vector3 moveDirection = Vector3.zero;
-    CharacterController controller;
+    //SphereCollider characterCollider;
+    //Rigidbody characterRigidbody;
+    //bool isGrounded;
 
+    //void Start()
+    //{
+    //    characterCollider = GetComponent<SphereCollider>();
+    //    characterRigidbody = GetComponent<Rigidbody>();
+    //}
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == ("Ground") && isGrounded == false)
+    //    {
+    //        isGrounded = true;
+    //    }
+    //}
+
+    //void FixedUpdate()
+    //{
+    //    int x;
+    //    if (Input.GetKey(right))
+    //        x = 1;
+    //    else if (Input.GetKey(left))
+    //        x = -1;
+    //    else
+    //        x = 0;
+
+    //    Vector3 movement = new Vector3(x, 0, 0);
+
+    //    characterRigidbody.AddForce(movement * speed);
+
+    //    if (isGrounded && Input.GetKeyDown(jump))
+    //    {
+    //        characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    //        isGrounded = false;
+    //    }
+    //}
+    #endregion
+
+    public float jumpSpeed = 5f;
+    public bool isGrounded;
+    Rigidbody rb;
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-        //anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
-
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
     void Update()
     {
-            //int x;
-            int x;
-
-            if (Input.GetKey(right))
-                x = 1;
-            else if (Input.GetKey(left))
-                x = -1;
-            else
-                x = 0;
-
-            moveDirection = new Vector3(x, 0, 0);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection = moveDirection * speed;
-
-            //if (Input.GetKey(up) || Input.GetKey(down) || Input.GetKey(left) || Input.GetKey(right))
-            //    anim.SetBool("walk", true);
-            //else
-            //    anim.SetBool("walk", false);
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
