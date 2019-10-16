d639c48b7546567e11ddb9dbecbdc3f9a4670dd6
BroSmithBoris <47716016+BroSmithBoris@users.noreply.github.com>
2019-10-16 23:26:53 +0500

    Сделал прыжок и передвижение

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,73 +6,53 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     #region
-    //KeyCode left = KeyCode.A;
-    //KeyCode right = KeyCode.D;
-    //KeyCode jump = KeyCode.Space;
+    KeyCode left = KeyCode.A;
+    KeyCode right = KeyCode.D;
+    KeyCode jump = KeyCode.Space;
 
-    //public float speed, jumpForce;
-    //public LayerMask groundLayers;
+    public float speed, speedLimit, jumpForce;
+    public LayerMask groundLayers;
 
-    //SphereCollider characterCollider;
-    //Rigidbody characterRigidbody;
-    //bool isGrounded;
+    SphereCollider characterCollider;
+    Rigidbody characterRigidbody;
+    bool isGrounded;
 
-    //void Start()
-    //{
-    //    characterCollider = GetComponent<SphereCollider>();
-    //    characterRigidbody = GetComponent<Rigidbody>();
-    //}
-
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.gameObject.tag == ("Ground") && isGrounded == false)
-    //    {
-    //        isGrounded = true;
-    //    }
-    //}
-
-    //void FixedUpdate()
-    //{
-    //    int x;
-    //    if (Input.GetKey(right))
-    //        x = 1;
-    //    else if (Input.GetKey(left))
-    //        x = -1;
-    //    else
-    //        x = 0;
-
-    //    Vector3 movement = new Vector3(x, 0, 0);
-
-    //    characterRigidbody.AddForce(movement * speed);
-
-    //    if (isGrounded && Input.GetKeyDown(jump))
-    //    {
-    //        characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    //        isGrounded = false;
-    //    }
-    //}
-    #endregion
-
-    public float jumpSpeed = 5f;
-    public bool isGrounded;
-    Rigidbody rb;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterCollider = GetComponent<SphereCollider>();
+        characterRigidbody = GetComponent<Rigidbody>();
     }
-    void OnCollisionEnter(Collision col)
+
+    void OnCollisionEnter(Collision collision)
     {
-        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        if (collision.gameObject.tag == ("Ground"))
         {
             isGrounded = true;
         }
     }
-    void Update()
+
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0) && isGrounded)
+        int x = 0;
+        if (characterRigidbody.velocity.x < speedLimit)
+            if (Input.GetKey(right))
+                x = 1;
+        else if (characterRigidbody.velocity.x > -speedLimit)
+            if (Input.GetKey(left))
+                x = -1;
+
+            Vector3 movement = new Vector3(x, 0, 0);
+
+            characterRigidbody.AddForce(movement * speed, ForceMode.Impulse);
+        
+
+        if (isGrounded && Input.GetKeyDown(jump))
         {
-            rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Impulse);
+            characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
     }
+    #endregion
+
+
 }
