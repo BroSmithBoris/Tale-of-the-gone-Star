144ddf353aa912df98042b37566798e03f49ea49
BroSmithBoris <47716016+BroSmithBoris@users.noreply.github.com>
2019-10-19 14:10:50 +0500

    Допилен до конца скрипт передвижения
    Убрана проблема с ускорением

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Move : MonoBehaviour
 {
-    #region
     KeyCode left = KeyCode.A;
     KeyCode right = KeyCode.D;
     KeyCode jump = KeyCode.Space;
 
-    public float speed, speedLimit, jumpForce;
+    public float speed, jumpForce;
     public LayerMask groundLayers;
 
-    SphereCollider characterCollider;
     Rigidbody characterRigidbody;
     bool isGrounded;
+    Vector3 movement;
 
     void Start()
     {
-        characterCollider = GetComponent<SphereCollider>();
         characterRigidbody = GetComponent<Rigidbody>();
     }
 
@@ -31,28 +29,33 @@ public class Move : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    void Update()
     {
         int x = 0;
-        if (characterRigidbody.velocity.x < speedLimit)
-            if (Input.GetKey(right))
-                x = 1;
-        else if (characterRigidbody.velocity.x > -speedLimit)
-            if (Input.GetKey(left))
-                x = -1;
+        if (Input.GetKey(right))
+            x = 1;
+        else if (Input.GetKey(left))
+            x = -1;
+        movement = new Vector3(x, 0, 0);
+    }
 
-            Vector3 movement = new Vector3(x, 0, 0);
+    void FixedUpdate()
+    {
+        CharacterJump(movement);
+        CharacterMove(movement);
+    }
 
-            characterRigidbody.AddForce(movement * speed, ForceMode.Impulse);
-        
-
+    void CharacterJump(Vector3 direction)
+    {
         if (isGrounded && Input.GetKeyDown(jump))
         {
             characterRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
     }
-    #endregion
 
-
+    void CharacterMove(Vector3 direction)
+    {
+        characterRigidbody.MovePosition((Vector3)transform.position + (direction * speed * Time.deltaTime));
+    }
 }
