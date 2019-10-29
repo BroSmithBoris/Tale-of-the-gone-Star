aabd52a8a95eae4fa7b54590448a9447113deb3c
Egorka0708 <egortush@yandex.ru>
2019-10-29 08:49:00 +0500

    Баловался, делая интерфейс

---
+++ Assets/Scripts/ToxicZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToxicZone : MonoBehaviour
+{
+    public Slider HealthPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            HealthPoint.value -= 0.001f;
+    }
+}
