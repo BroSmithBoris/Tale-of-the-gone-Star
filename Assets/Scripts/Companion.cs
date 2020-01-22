c5d44d0f9b38077505ebd569834cd28451803b97
Egorka0708 <egortush@yandex.ru>
2020-01-22 17:47:29 +0500

    Залил пробного спутника
    у нас всё очень плохо....

---
+++ Assets/Scripts/Companion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Companion : MonoBehaviour
+{
+    NavMeshAgent friend;
+    GameObject player;
+    public float distance;
+    public float speed;
+    public float speedRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        friend = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) > distance)
+        {
+            Vector3 rotation = player.transform.position - friend.transform.position;
+            friend.transform.rotation = Quaternion.Slerp(friend.transform.rotation, Quaternion.LookRotation(rotation), speedRotation * Time.deltaTime);
+            friend.transform.position += friend.transform.forward * speed * Time.deltaTime;
+            friend.transform.localPosition = new Vector3(transform.position.x, transform.position.y, 5.5f);
+            friend.transform.localRotation = new Quaternion(0f, transform.position.y, 0f, transform.rotation.w);
+        }
+    }
+}
